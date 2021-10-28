018ef9b90ae99a77e319f2ca3ac8215f1dbe0c6e
PrithwishNag <prithwish.nag@gmail.com>
2021-10-28 16:32:55 +0530

    Pop up!

--- address-update/src/components/Form.js
+++ address-update/src/components/Form.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 export class Form extends Component {
+  input = {
+    display: "inline-block",
+    border: "0px",
+    borderBottom: "1px solid black",
+    outline: "none",
+  };
+
+  otp() {
+    const box = {
+      width: "100%",
+      paddingTop: "10px",
+      paddingBottom: "10px",
+      textAlign: "center",
+    };
+    return (
+      <form style={box}>
+        <div>OTP</div>
+        <div style={{ marginBottom: 10 }}>
+          <input style={this.input} />
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    );
+  }
+
   render() {
     const box = {
       margin: "auto",
-      width: "300px",
+      width: "400px",
       padding: "20px",
       borderRadius: "10px",
       border: "5px solid grey",
-    };
-
-    const input = {
-      display: "inline-block",
-      border: "0px",
-      borderBottom: "1px solid black",
-      outline: "none",
     };
 
     const submit = {
@@ -26,7 +48,6 @@ export class Form extends Component {
     const topic = {
       display: "inline-block",
       width: 75,
-      //   backgroundColor: "red",
       textAlign: "left",
       fontSize: "20px",
     };
@@ -36,31 +57,38 @@ export class Form extends Component {
     };
 
     return (
-      <form style={box} autoComplete="off">
-        <div style={row}>
-          <span style={topic}>Name</span> :{" "}
-          <input
-            style={input}
-            placeholder="Ramesh"
-            pattern="[\w\s]+"
-            autoComplete="off"
-            required
-          />
-        </div>
-        <div style={row}>
-          <span style={topic}>Aadhar</span> :{" "}
-          <input
-            style={input}
-            placeholder="0000 0000 0000"
-            pattern="\d{4}\s\d{4}\s\d{4}"
-            autoComplete="off"
-            required
-          />
-        </div>
+      <div style={box}>
+        <form autoComplete="off">
+          <div style={row}>
+            <span style={topic}>Name</span> :{" "}
+            <input
+              style={this.input}
+              placeholder="Ramesh"
+              pattern="[\w\s]+"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div style={row}>
+            <span style={topic}>Aadhar</span> :{" "}
+            <input
+              style={this.input}
+              placeholder="0000 0000 0000"
+              pattern="\d{4}\s\d{4}\s\d{4}"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div style={row}>
+            <input type="submit" style={submit} value="Generate OTP" />
+          </div>
+        </form>
         <div>
-          <input type="submit" style={submit} value="Request for OTP" />
+          <Popup trigger={<button style={submit}>Submit OTP</button>} modal>
+            {this.otp()}
+          </Popup>
         </div>
-      </form>
+      </div>
     );
   }
 }
