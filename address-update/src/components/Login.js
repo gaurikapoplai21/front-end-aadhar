5d702ae0de63eb33e0299935b8cef45806e3fbff
PrithwishNag <prithwish.nag@gmail.com>
2021-10-28 15:28:45 +0530

    Form

--- address-update/src/components/Login.js
+++ address-update/src/components/Login.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
-import Navbar from './Navbar.js'
-import Form from './Form.js'
-
-
+import React, { Component } from "react";
+import Navbar from "./Navbar.js";
+import Form from "./Form.js";
 
 export class Login extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <Form />
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div>Login as</div>
+        <div style={{ marginBottom: "30px", fontSize: "40px" }}>Requester</div>
+        <Form />
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
