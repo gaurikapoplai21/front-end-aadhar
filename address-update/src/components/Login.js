c3d0ef473b7b6392e115a4cca395abd6cc8073c7
gaurika poplai <gaurikapoplai@gmail.com>
2021-10-28 17:18:20 +0530

    small css change

--- address-update/src/components/Login.js
+++ address-update/src/components/Login.js
@@ -22,7 +22,7 @@ export class Login extends Component
     return (
       <div>
         <Navbar logintype={this.handleCallback}/>
-        <div style={{"margin-top":"30px"}}>Login as</div>
+        <div style={{"margin-top":"30px","font-size":"30px"}}>Login as</div>
         <div style={{ marginBottom: "30px", fontSize: "40px" }}>{this.state.logintype}</div>        
         <Form />
       </div>
