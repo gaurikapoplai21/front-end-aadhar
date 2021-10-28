76425f33854362a282c05979d33887bc19aed875
gaurika poplai <gaurikapoplai@gmail.com>
2021-10-28 13:09:01 +0530

    components

---
+++ address-update/src/components/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar.js'
+import Form from './Form.js'
+
+
+
+export class Login extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Form />
+
+            </div>
+        )
+    }
+}
+
+export default Login
