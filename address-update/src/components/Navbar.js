295776ddd87af4642a21978a80d060796d9caa87
PrithwishNag <prithwish.nag@gmail.com>
2021-10-30 12:47:58 +0530

    Slight changes

--- address-update/src/components/Navbar.js
+++ address-update/src/components/Navbar.js
@@ -1,46 +1,45 @@
 import React, { Component } from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 
-export class NavBar extends Component 
-{
-        constructor(props) {
-            super(props);
-            this.state = {
-                logintype: "Landlord"
-            };
+class LoginNavbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logintype: "Landlord",
+    };
+  }
 
-        }
-        loginchange = () =>{
-            if(this.state.logintype==="Landlord")
-            {
-                this.setState({logintype:"Requester"})
-            }
-            else
-            {
-                this.setState({ logintype: "Landlord" })
-            }
-            this.props.logintype(this.state.logintype)
-        }
-    render() {
-        return (
-            <div>
-                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="#home">Address-Update</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            
-                            <Nav>
-                
-                                <Nav.Link  style={{"margin-left":"60vw"}} onClick={this.loginchange}>Login as {this.state.logintype}</Nav.Link>
-                               
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        )
+  loginchange = () => {
+    if (this.state.logintype === "Landlord") {
+      this.setState({ logintype: "Requester" });
+    } else {
+      this.setState({ logintype: "Landlord" });
     }
+    this.props.logintype(this.state.logintype);
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Address-Update</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav>
+                <Nav.Link
+                  style={{ "margin-left": "60vw" }}
+                  onClick={this.loginchange}
+                >
+                  Login as {this.state.logintype}
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default NavBar;
+export { LoginNavbar };
